fce7f2bd1921e54e281226bea509c5b0af83e133
Your Name <you@example.com>
2025-07-29 18:42:38 +0530

    initial commit

---
+++ src/main/java/com/mytrandingai/ai_trading_platform/AiTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mytrandingai.ai_trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTradingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytrandingai/ai_trading_platform/AiTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytrandingai.ai_trading_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
